606f260335c15543b682367c93ba5877ed026e87
Sam Killgallon <s.killgallon@livelinktechnology.net>
2017-12-27 20:21:15 +0000

    Update capistrano version lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 # config valid only for current version of Capistrano
-lock "3.10.0"
+lock '3.10.1'
 
-set :application, "archery"
-set :repo_url, "git@github.com:Sam-Killgallon/archery_tracker.git"
+set :application, 'archery'
+set :repo_url, 'git@github.com:Sam-Killgallon/archery_tracker.git'
 set :user, 'deploy'
 
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 set :puma_init_active_record, true
