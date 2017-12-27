141bf2c7af86729482ada6c9e0ea02744d3fa88a
Sam Killgallon <s.killgallon@livelinktechnology.net>
2017-12-27 22:15:34 +0000

    Set deploy user for prod

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,4 @@
 # ======================
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-server 'vps', roles: %w[app db web]
-
+server 'vps', user: 'deploy', roles: %w[app db web]
